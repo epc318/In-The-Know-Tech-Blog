3d949fe042b524dd274109e33ab5c53418091f95
Rick Johnson <carl4917@umn.edu>
2022-03-04 23:39:11 -0600

    adding parameters to server.js

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
+const expHandlebar = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
+const sess = {
+    secret: "Super duper secret stuff",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const handlebars = expHandlebar.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+
+
+
+app.use(routes);
+
+// acitvate server and database connection
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Application Now listening'));
+  });
+  
