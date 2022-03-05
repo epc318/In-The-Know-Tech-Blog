9f508bbee465b0007eb23f7097f7430ed699f05b
Rick Johnson <carl4917@umn.edu>
2022-03-04 21:49:38 -0600

    creating user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
+const commentRoute = require("./comment-routes");
+const postRoute = require("./post-routes");
 const userRoute = require("./user-routes");
 
 
-
-
+router.use("/comments", commentRoute);
+router.use("/posts", postRoute);
+router.use("/users", userRoute);
 
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,1 +1,52 @@
 const router = require("express").Router();
+const { user, post, comment } = require("../../models");
+
+
+// get all users
+router.get("/", (req, res) => {
+    user.findAll({
+        attributes: { exclude: ["password"] }
+    })
+        .then(userInfo => res.json(userInfo))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+router.get("/:id", (req, res) => {
+    user.findOne({
+        attributes: { exclude: ["password"] },
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(userInfo => {
+            if(!userInfo) {
+                res.status(404).json({ message: "This user ID does not exist, please check input and try again" });
+                return;
+            }
+            res.json(userInfo);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// create user/profile
+router.post("/", (req, res) => {
+    user.create({
+        userID: req.body.userID,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(userInfo => res.json(userInfo))
+        .catch(err => {
+            console.log(err);
+            res.status(404).json(err);
+        })
+})
+
+
+module.exports = router;
