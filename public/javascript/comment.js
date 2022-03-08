002f9ff16a22478d546b347690ded75983909bf4
Rick Johnson <carl4917@umn.edu>
2022-03-07 19:50:59 -0600

    adding comment functionality

---
+++ public/javascript/comment.js
@@ -0,0 +1,27 @@
+async function comment(event) {
+    event.preventDefault();
+    const comment_input = document.querySelector("").value.trim();
+    const post_id = window.location.toString().split("/")
+    [
+        window.location.toString().split("/").length - 1
+    ];
+
+    if(comment_input) {
+        const response = await fetch("/api/comments", {
+            method: "Post",
+            body: JSON.stringify({
+                post_id,
+                comment_input
+            }),
+            headers: {
+                "credential": "appJson"
+            }
+        });
+
+        if(response.ok) {
+            document.location.reload();
+        }
+    }
+}
+
+//*add comment form to query selctor document.querySelector("").addEventListener("submit", comment);
