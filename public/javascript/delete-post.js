76628c139245c94b506dad3a7edda23668688987
Rick Johnson <carl4917@umn.edu>
2022-03-07 19:33:49 -0600

    several edits, homepage handlbars, adding delete post functionality, editing main handlebars

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,17 @@
+async function deletePost(event) {
+    event.preventDefault();
+
+    const ID = window.location.toString().split("/")
+    [
+      window.location.toString().split("/").length - 1
+    ];
+
+    const respond = await fetch(`/api/posts/${ID}`, {
+        method: "Delete"
+    });
+    if (respond.ok) {
+        document.location.replace("/dashboard/");
+    }
+  }
+
+ // *add delete form/class to query selctor document.querySelector("").addEventListener("click", deletePost);
