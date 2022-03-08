8460a17e1f7c1f7e05e9da6cce451f847929ecc1
Rick Johnson <carl4917@umn.edu>
2022-03-07 19:41:51 -0600

    finishing server.js code, small edit comment handlebars

--- server.js
+++ server.js
@@ -23,10 +23,17 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess)); 
 
 
-
+// turn on routes
 app.use(routes);
+
 
 // acitvate server and database connection
 sequelize.sync({ force: false }).then(() => {
